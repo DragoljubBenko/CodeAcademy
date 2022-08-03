c81bb5e72d62906fe2dc952d32cbac21b32e28bb
DragoljubBenko <dragoljub.benko@yahoo.com>
2022-08-03 18:05:40 +0200

    java

---
+++ JavaScript/easyLet.js
@@ -0,0 +1,24 @@
+let name1 = "Dragoljub";
+let surname1 = "Benko";
+let city1 = "Skopje";
+let age1 = "31";
+let job1 = "student";
+
+// Objekt
+
+let user = {
+  name: name1,
+  surname: surname1,
+  city: city1,
+  age: age1,
+  job: job1,
+};
+
+console.log(user);
+function myFunction() {
+  alert(" Name Surname ");
+}
+// let btn = document.getElementsByTagName("button")[0];
+// btn.addEventListener("click", function () {
+//   alert("ako nema alert nema da se ukluci ");
+// });
