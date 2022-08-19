d233b95127c321f818dc90b5cabe0c423f6122d1
DragoljubBenko <dragoljub.benko@yahoo.com>
2022-08-19 12:42:35 +0200

    stillMissingTwoFunction

---
+++ JavaScript/loopsAndArray.js
@@ -0,0 +1,5 @@
+// let function who contains two parametars and with pop element
+
+let thatAcceptsTwoParametars = [[1, 23, 456], [1]];
+thatAcceptsTwoParametars.pop();
+console.log(thatAcceptsTwoParametars);
